bae852a21630d821cbf7442c712233e66b5e7a41
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-13 11:51:50 +0930

    utils folder updated.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,5 @@
 function generateMarkdown(data) {
   return `
-  # static-site [![NPM version](https://badge.fury.io/js/static-site.svg)](https://npmjs.org/package/static-site) [![Build Status](https://travis-ci.org/RumaRDas/static-site.svg?branch=master)](https://travis-ci.org/RumaRDas/static-site)
 # ${data.title}
 
 `;
